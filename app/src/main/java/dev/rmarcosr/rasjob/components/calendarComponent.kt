b070aeb5dfbacc6ea95ac2fbd6b8a81d398d1f39
Marcos Rodríguez <marcosrodriguezroman700.02@gmail.com>
2025-07-25 19:27:46 +0200

    feat: Implement Date and Time Picker Components

---
+++ app/src/main/java/dev/rmarcosr/rasjob/components/calendarComponent.kt
@@ -0,0 +1,90 @@
+package dev.rmarcosr.rasjob.components
+
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+import dev.rmarcosr.rasjob.viewmodels.MainViewModel
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+@Composable
+fun DatePickerFieldToModal(mainViewModel : MainViewModel, modifier: Modifier = Modifier) {
+    var selectedDate by remember { mutableStateOf<Long?>(null) }
+    var showModal by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = selectedDate?.let { convertMillisToDate(it) } ?: "",
+        onValueChange = { },
+        label = { Text("Día") },
+        placeholder = { Text("") },
+        trailingIcon = {
+            Icon(Icons.Default.DateRange, contentDescription = "Selecciona un día")
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .pointerInput(selectedDate) {
+                awaitEachGesture {
+                    awaitFirstDown(pass = PointerEventPass.Initial)
+                    val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+                    if (upEvent != null) {
+                        showModal = true
+                    }
+                }
+            }
+    )
+
+    if (showModal) {
+        DatePickerModal(
+            mainViewModel = mainViewModel,
+            onDateSelected = {
+                selectedDate = it
+                mainViewModel.day = convertMillisToDate(it) },
+            onDismiss = { showModal = false }
+        )
+    }
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("dd/MM/yyyy", Locale("es"))
+    return formatter.format(Date(millis))
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    mainViewModel: MainViewModel,
+    onDateSelected: (Long) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val state = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                state.selectedDateMillis?.let { onDateSelected(it) }
+                onDismiss()
+            }) {
+                Text("Aceptar")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancelar")
+            }
+        }
+    ) {
+        DatePicker(state = state)
+    }
+}
